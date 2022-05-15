e265734d4634ae6099ffb8c720800176d475f497
MasterTM2022 <perov.public@gmail.com>
2022-05-15 20:13:15 +0300

    add task 5(2) (les5) summ of digit [N] with recurcive F.

---
+++ L5_T6_2Rec_SummDigit/SummOfDigits.java
@@ -0,0 +1,18 @@
+package L5_T6_2Rec_SummDigit;
+
+import java.util.Scanner;
+
+public class SummOfDigits {
+    public static void main(String[] arg) {
+        System.out.println("Введите число:");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("Результат:" + summ(n));
+    }
+        private static int summ(int x){
+            if (x<10 & x%10 == 0) {
+                return 0;
+            }
+            return x%10 + summ(x/10);
+        }
+    }
