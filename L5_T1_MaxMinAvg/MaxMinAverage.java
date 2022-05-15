b4665c39f300e05b54cc97ae5ff606531f872d77
MasterTM2022 <perov.public@gmail.com>
2022-05-15 11:07:39 +0300

    Лекция 5, Задача 2, сортировка (три метода - выбор, вставка, слияние)

---
+++ L5_T1_MaxMinAvg/MaxMinAverage.java
@@ -0,0 +1,40 @@
+package L5_T1_MaxMinAvg;
+
+public class MaxMinAverage {
+    public static void main(String[] args) {
+            int[] array = {
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+                    (int) (Math.random()*100),
+            };
+            int min=array[0];
+            int max=array[0];
+            int sum = 0;
+            int count = 0;
+            String mass = "";
+            for (int i : array) {
+                mass = mass + i + "; ";
+                if (min > i) {
+                    min = i;
+                } else if
+                (max < i) {
+                    max = i;
+                }
+                sum += i;
+                ++count;
+            }
+            System.out.println("массив: " + mass);
+            System.out.println("максимум = " + max);
+            System.out.println("минимум = " + min);
+            System.out.println("среднее = " + String.format("%.2f", (double) sum / count));
+            System.out.println("сумма = " + sum);
+        }
+
+}
