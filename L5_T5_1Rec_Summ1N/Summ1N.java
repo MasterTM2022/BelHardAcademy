2f638deae9ce215a9d9508d9053e8ce4a7fda701
MasterTM2022 <perov.public@gmail.com>
2022-05-15 19:54:40 +0300

    add task 5(1) (les5) summ [1;N] with recurcive F.

---
+++ L5_T5_1Rec_Summ1N/Summ1N.java
@@ -0,0 +1,18 @@
+package L5_T5_1Rec_Summ1N;
+
+import java.util.Scanner;
+
+public class Summ1N {
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("Результат:" + summ(n));
+    }
+        private static int summ(int x){
+            if (x == 0) {
+                return 0;
+            }
+            return x + summ(x - 1);
+        }
+    }
